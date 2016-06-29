cf1174a16917bbdb66db70a0e3a8df7b02d43778
Frantisek Kobzik <fkobzik@suse.de>
2016-06-29 12:27:33 +0200

    some useless stuff

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject blogiseq "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple databaseless blogging"
+  :url "todo github"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
